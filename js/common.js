a7b2903531d1ceb3ffb56b9afdaaaa160cef594b
moudoder <39887984+moudoder@users.noreply.github.com>
2021-10-21 23:48:44 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
   new WOW().init();
+  $('.header-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.header-arrow__left'),
+    nextArrow: $('.header-arrow__next'),
+   });
   
 })
