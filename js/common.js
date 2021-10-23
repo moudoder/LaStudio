ebec9ae9f3e7e9d72e345225d92c8b1894a7de4c
moudoder <39887984+moudoder@users.noreply.github.com>
2021-10-23 19:35:09 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -10,5 +10,25 @@ $(document).ready(function () {
     prevArrow: $('.header-arrow__left'),
     nextArrow: $('.header-arrow__next'),
    });
-  
+  $('.menu-img').on('click', function() {
+    $('.modal-menu').addClass('modal-menu-active');
+    $('.dark-window').addClass('dark-window-active');
+  })
+  $('.modal-menu-list__item_close').on('click', function() {
+    $('.modal-menu').removeClass('modal-menu-active');
+    $('.dark-window').removeClass('dark-window-active');
+    return false;
+  })
+  $('.services__btn').on('click', function() {
+    $('.modal-form').addClass('modal-form-active');
+    $('.dark-window').addClass('dark-window-active');
+    return false;
+  })
+  $('.dark-window').on('click', function() {
+    $('.modal-menu').removeClass('modal-menu-active');
+    $('.modal-form').removeClass('modal-form-active');
+    $('.dark-window').removeClass('dark-window-active');
+    return false;
+  })
+
 })
